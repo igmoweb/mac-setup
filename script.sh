3689dbce283d41167b4484f8df9be2bc968fd730
Ignacio Cruz <igmoweb@gmail.com>
2023-01-25 13:38:43 +0100

    2023 updates

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ========== Homebrew ==========
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 
 # ========== Git & svn ==========
